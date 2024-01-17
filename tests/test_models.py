fa152d178621a69ab4a1b5375a31477e832e1018
UrbanSwati <williesdk@gmail.com>
2024-01-17 09:09:56 +0200

    fixed failing model test

--- tests/test_models.py
+++ tests/test_models.py
@@ -12,7 +12,7 @@ def test_announcement_model():
 
     model = Announcement(**raw_model)
 
-    assert model.phone_numbers == ["test@gmail.com", "test2@gmail.com", "test@gmail.com"]
+    assert model.phone_numbers == ["12345", "123456", "123457"]
     assert model.subject == "test"
     assert model.message == "test"
     assert model.send_time == datetime(2021, 3, 1, 0, 0, 0)
